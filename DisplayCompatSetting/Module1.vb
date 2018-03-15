d26c70dcc28fb1c1afbf5239e0597643878db8e2
Ferry <ferry.temaja.putra@gmail.com>
2018-03-15 13:47:40 +0800

    Complete the validation.

--- DisplayCompatSetting/Module1.vb
+++ DisplayCompatSetting/Module1.vb
@@ -6,9 +6,9 @@ Module Module1
 
     Private Const DPISettingValue = "~ GDIDPISCALING DPIUNAWARE"
     Private Const CompatibilitySettingKeyName = "HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers"
-    Private ReadOnly AppFileName = Assembly.GetExecutingAssembly.Location
-    Private Const WindowsCaption = "Windows 10"
-    Private Const WindowsVersion = ""
+    Private ReadOnly AppFileName As String = Assembly.GetExecutingAssembly.Location
+    Private Const Windows10Caption = "Windows 10"
+    Private Const FallCreatorsUpdateBuildNumber = 16299
 
     Sub Main()
 
@@ -28,22 +28,39 @@ Module Module1
 
     End Sub
 
-    Private Function IsOverrideHighDPIScalingSupported()
+    Private Function IsOverrideHighDPIScalingSupported() As Boolean
 
         Console.WriteLine("Checking OS version...")
 
-        Dim searcher As New ManagementObjectSearcher("SELECT Caption, Version FROM Win32_OperatingSystem")
+        Dim searcher As New ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem")
+
+        Dim osCaption = String.Empty
+        Dim osVersion = String.Empty
+        Dim osBuildNumber = String.Empty
 
         For Each result In searcher.Get
-            Console.WriteLine("OS Version: {0} {1}", result("Caption"), result("Version"))
+            osCaption = CType(result("Caption"), String)
+            osBuildNumber = CType(result("BuildNumber"), String)
+            osVersion = CType(result("Version"), String)
+            Exit For
         Next
 
+        Console.WriteLine("OS: {0} {1} {2}", osCaption, osBuildNumber, osVersion)
         Console.WriteLine()
 
-        Return True
+        Dim isSupported =
+            osCaption.Contains(Windows10Caption) AndAlso
+            CType(osBuildNumber, Integer) >= FallCreatorsUpdateBuildNumber
+
+        If Not isSupported Then
+            Console.WriteLine("Current OS does not support overriding high dpi scaling setting.")
+        End If
+
+        Return isSupported
+
     End Function
 
-    Private Function IsHighDPISettingConfigured()
+    Private Function IsHighDPISettingConfigured() As Boolean
 
         Console.WriteLine("Checking High DPI Scaling setting...")
 
